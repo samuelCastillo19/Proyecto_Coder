8018a86d4c472b29b4dc4e1b47e184fd62f96bf5
samuelCastillo19 <samuelcastillo@MacBook-Pro-de-Samuel.local>
2022-01-13 20:49:37 -0600

    Punto Seguro AppCoder

---
+++ AppCoder/models.py
@@ -0,0 +1,26 @@
+import email
+from django.db import models
+
+class Curso(models.Model):
+    
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+
+class Estudiante(models.Model):
+    
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    
+class Profesor(models.Model):
+    
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=40)
+    
+class Entregable(models.Model):
+    
+    nombre = models.CharField(max_length=40)
+    fecha_entrega = models.DateField()
+    entregado = models.BooleanField()
