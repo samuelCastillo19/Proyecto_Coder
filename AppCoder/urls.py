629d0eabe25cb13485003b643e81c437be2ffdb3
samuelCastillo19 <samuelcastillo@MacBook-Pro-de-Samuel.local>
2022-01-15 11:52:03 -0600

    Barra navegacion lista, html padre creado

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from AppCoder import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path('', views.inicio, name='Inicio'),
     path('cursos', views.cursos, name='Cursos'),
     path('profesores', views.profesores, name='Profesores'),
     path('estudiantes', views.estudiantes, name='Estudiantes'),
