216b461111bfdef4d680de3f5164965050e0169f
samuelCastillo19 <samuelcastillo@MacBook-Pro-de-Samuel.local>
2022-01-15 08:44:01 -0600

    Templates y carpeta static, bootstrap corriendo

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -3,20 +3,20 @@ from django.http import HttpResponse
 
 def inicio(request):
     
-    return HttpResponse('Vista inicio')
+    return render(request, "AppCoder/inicio.html")
 
 def cursos(request):
     
-    return HttpResponse('Vista Cursos')
+    return render(request, "AppCoder/cursos.html")
 
 def profesores(request):
     
-    return HttpResponse('Vista Profesores')
+    return render(request, "AppCoder/profesores.html")
 
 def estudiantes(request):
     
-    return HttpResponse('Vista Estudiantes')
+    return render(request, "AppCoder/estudiantes.html")
 
 def entregables(request):
     
-    return HttpResponse('Vista Entregables')
+    return render(request, "AppCoder/entregables.html")
