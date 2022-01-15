2adfef3167d5f5563cee72ecd10219983e19deb8
samuelCastillo19 <samuelcastillo@MacBook-Pro-de-Samuel.local>
2022-01-15 16:16:02 -0600

    App acorde con clase 20

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -5,6 +5,9 @@ class Curso(models.Model):
     
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
+    
+    def __str__(self):
+        return f'{self.nombre}'
 
 class Estudiante(models.Model):
     
@@ -12,12 +15,18 @@ class Estudiante(models.Model):
     apellido = models.CharField(max_length=40)
     email = models.EmailField()
     
+    def __str__(self):
+        return f'{self.apellido}, {self.nombre}'
+    
 class Profesor(models.Model):
     
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
     email = models.EmailField()
     profesion = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return f'{self.apellido}, {self.nombre}'
     
 class Entregable(models.Model):
     
