cae86a049da14449a0a6c11bd4798aaf0549a8a3
samuelCastillo19 <samuelcastillo@MacBook-Pro-de-Samuel.local>
2022-01-29 11:16:12 -0600

    Se agregan botonesa formularios

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,10 +3,10 @@ from AppCoder import views
 
 urlpatterns = [
     path('', views.inicio, name='Inicio'),
-    path('cursos', views.cursos, name='Cursos'),
-    path('profesores', views.profesores, name='Profesores'),
-    path('estudiantes', views.estudiantes, name='Estudiantes'),
-    path('entregables', views.entregables, name='Entregables'),
+    path('cursos/', views.cursos, name='Cursos'),
+    path('profesores/', views.profesores, name='Profesores'),
+    path('estudiantes/', views.estudiantes, name='Estudiantes'),
+    path('entregables/', views.entregables, name='Entregables'),
     path('formulario_curso/', views.formulario_curso, name='formulario_curso'),
     path('formulario_profesor/', views.formulario_profesor, name='formulario_profesor')
 ]
